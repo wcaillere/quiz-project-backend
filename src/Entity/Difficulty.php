b4608e5c3ef490bb817792ad0c99f6421baadf2a
Administrateur <wcaillere.pro@gmail.com>
2023-07-12 12:00:15 +0200

    Revert "Création de l'entité Quiz et de ses relations"
    This reverts commit 83d8715191443cc700c161e4ae00a99ff3f0b7a8.

--- src/Entity/Difficulty.php
+++ src/Entity/Difficulty.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\DifficultyRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DifficultyRepository::class)]
@@ -17,14 +15,6 @@ class Difficulty
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
-
-    #[ORM\OneToMany(mappedBy: 'difficulty', targetEntity: Quiz::class)]
-    private Collection $quizzes;
-
-    public function __construct()
-    {
-        $this->quizzes = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -39,36 +29,6 @@ class Difficulty
     public function setName(string $name): static
     {
         $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Quiz>
-     */
-    public function getQuizzes(): Collection
-    {
-        return $this->quizzes;
-    }
-
-    public function addQuiz(Quiz $quiz): static
-    {
-        if (!$this->quizzes->contains($quiz)) {
-            $this->quizzes->add($quiz);
-            $quiz->setDifficulty($this);
-        }
-
-        return $this;
-    }
-
-    public function removeQuiz(Quiz $quiz): static
-    {
-        if ($this->quizzes->removeElement($quiz)) {
-            // set the owning side to null (unless already changed)
-            if ($quiz->getDifficulty() === $this) {
-                $quiz->setDifficulty(null);
-            }
-        }
 
         return $this;
     }
--- src/Entity/Theme.php
+++ src/Entity/Theme.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\ThemeRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ThemeRepository::class)]
@@ -17,14 +15,6 @@ class Theme
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
-
-    #[ORM\ManyToMany(targetEntity: Quiz::class, mappedBy: 'themes')]
-    private Collection $quizzes;
-
-    public function __construct()
-    {
-        $this->quizzes = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -39,33 +29,6 @@ class Theme
     public function setName(string $name): static
     {
         $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Quiz>
-     */
-    public function getQuizzes(): Collection
-    {
-        return $this->quizzes;
-    }
-
-    public function addQuiz(Quiz $quiz): static
-    {
-        if (!$this->quizzes->contains($quiz)) {
-            $this->quizzes->add($quiz);
-            $quiz->addTheme($this);
-        }
-
-        return $this;
-    }
-
-    public function removeQuiz(Quiz $quiz): static
-    {
-        if ($this->quizzes->removeElement($quiz)) {
-            $quiz->removeTheme($this);
-        }
 
         return $this;
     }
